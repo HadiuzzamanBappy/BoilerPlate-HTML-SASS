b7305c87bb58926b66aad7b9a52535a2dbb080a7
HadiuzzamanBappy <dbappy411@gmail.com>
2025-02-14 05:04:03 +0600

    sass structure modified

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
   $("#main-nav").load("../../pages/components/navbar.html");
-  $("#main-toolbar").load("../../pages/components/header.html");
+  $("#main-header").load("../../pages/components/header.html");
   $("#main-content").load("../../pages/components/mui-element.html");
 });
